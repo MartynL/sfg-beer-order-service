77dd2d8c5184f9ce61db89aded1623daf88152f9
mlatta <martyn.latta@bipsolutions.com>
2020-06-26 16:51:51 +0100

    initial commit

---
+++ src/main/java/com/mlatta/beerorder/SfgBeerOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mlatta.beerorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBeerOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgBeerOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlatta/beerorder/SfgBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlatta.beerorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgBeerOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
