66b3abe09ef5ce76ed958c51c94898ece05c357f
mlatta <martyn.latta@bipsolutions.com>
2020-08-28 15:17:15 +0100

    Added upc to the beerOrderLine domain object

--- src/main/java/com/mlatta/beerorder/domain/BeerOrderLine.java
+++ src/main/java/com/mlatta/beerorder/domain/BeerOrderLine.java
@@ -18,15 +18,17 @@ public class BeerOrderLine extends BaseEntity {
 
     @ManyToOne
     private BeerOrder beerOrder;
-
+    
+    private String upc;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
     
     @Builder
     public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
-                         BeerOrder beerOrder, Integer orderQuantity,
+                         BeerOrder beerOrder, String upc, Integer orderQuantity,
                          Integer quantityAllocated) {
         super(id, version, createdDate, lastModifiedDate);
+        this.upc = upc;
         this.beerOrder = beerOrder;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
