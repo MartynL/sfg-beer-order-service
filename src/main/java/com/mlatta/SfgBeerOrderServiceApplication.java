3516735cb9d5337134ff2648c5020db07b7e25cc
Martyn Latta <mlatta1990@hotmail.co.uk>
2021-07-01 19:31:42 +0100

    adding mysql to project and fixing some issues.

--- src/main/java/com/mlatta/SfgBeerOrderServiceApplication.java
+++ src/main/java/com/mlatta/SfgBeerOrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.mlatta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {EurekaClientAutoConfiguration.class, EurekaDiscoveryClientConfiguration.class})
 public class SfgBeerOrderServiceApplication {
 
 	public static void main(String[] args) {
